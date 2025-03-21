5a3a1692be1f3e89d92d4645f004c6055cb34940
prince356 <princechaurasia014@gmail.com>
2025-03-21 10:54:52 +0530

    Calculate Employee Wage

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //UC 1
 const IS_Absent = 0;
- 
+
 let empCheck = Math.floor(Math.random()* 10) % 2;
 
 if(empCheck == IS_Absent){
@@ -10,3 +10,27 @@ if(empCheck == IS_Absent){
 else{
     console.log("Employee is Present");
 }
+
+//UC2 Calculating the Employee Wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 4;
+
+empCheck = Math.floor(Math.random()* 10) % 3;
+
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    default:
+        empHrs = 4;
+}
+let empWage = empHrs * WAGE_PER_HOUR ;
+console.log("Emp Wage: "  +empWage)
