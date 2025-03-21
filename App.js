0bd8547fcc5ca83e95cc54b7914bc7eb479b5077
prince356 <princechaurasia014@gmail.com>
2025-03-21 11:07:01 +0530

    added Wages condition for month

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const Num_OF_WORKING_DAYS = 20;
+const Num_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
 let empCheck = Math.floor(Math.random()* 10) % 2;
 
@@ -28,9 +31,11 @@ function getWorkingHours(empCheck){
 }
 let empHrs = 0;
 
-for (let day = 0; day < Num_OF_WORKING_DAYS; day++) {
-        empCheck = Math.floor(Math.random()* 10) % 3;
-        empHrs += getWorkingHours(empCheck);
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    empCheck = Math.floor(Math.random()* 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR ;
-console.log("Emp Wage: "  +empWage)
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR ;
+console.log("UC5 - Total Days:- " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
